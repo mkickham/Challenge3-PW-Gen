441b91059e085590c9ef627ae4a061e24621d82c
Michael Kickham <mkickham@TFS.local>
2023-10-12 14:58:03 -0500

    Updated the code

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,4 @@
-// Assignment code here
-
-
-// Get references to the #generate element
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -15,3 +12,13 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword(){
+  var length = parseInt(prompt("Enter a password that is 10-12 symbols in length"));
+
+}
+
+var SpecChar = confirm("Enter a special character")
+var NumChar = confirm("Enter a number")
+var UpperCase = confirm("Enter an uppercase letter")
+var LowerCase = confirm("Enter a lower case letter")
